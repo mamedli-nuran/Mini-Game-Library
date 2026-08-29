4794d04ec7fa52f89efdfbc77c6e49645c74e1bd
Mamedli Nuran <mamedlinuran0006@mail.ru>
2026-08-29 16:17:18 +0400

    add user migration and model

---
+++ migrations/000001_create_users.up.sql
@@ -0,0 +1,14 @@
+BEGIN;
+
+CREATE TABLE users
+(
+    id         UUID PRIMARY KEY,
+    username   VARCHAR(255) NOT NULL,
+    email      VARCHAR(255) NOT NULL,
+    password   BYTEA        NOT NULL,
+    created_at TIMESTAMP    NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP    NOT NULL DEFAULT NOW(),
+    UNIQUE (username, email)
+);
+
+COMMIT;
---
+++ models/models.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	Id        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
